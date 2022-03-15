ffcded57cfed55147ddd945d6d76e02fcc69c930
FinnMx <93927783+FinnMx@users.noreply.github.com>
2022-03-15 10:37:35 +0000

    added login foundation with no backend
    yes

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+      <form style="width: 175px; text-align: center; position: absolute; top: 35%; left: 45%;" method="post"> 
+
+      <h2>Login</h2> 
+
+      <div class="form-group">
+        <input type="text" placeholder="Username" name="username" class="form-control"> 
+      </div>
+
+      <div class="form-group">
+        <input type="password" placeholder="Password" name ="password" class="form-control"> 
+      </div>
+
+        <input type="submit" value="Login" name="submit" class="btn btn-primary btn-sm"> 
+
+      </form> 
+
+      //method to check login not created properly yet ygm
+
+      <?php
+      error_reporting(0);
+
+      if (isset($_POST['submit'])){
+
+          if($_POST['username']== '' || $_POST['password'] == ''){
+                echo "Please fill all fields";
+          }
+
+          else{
+            $db = new SQLite3('C:\xampp\htdocs\myDB.db');
+            $sql = "SELECT Username, Password FROM Users WHERE Username =:username";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':username', $_POST['username'], SQLITE3_TEXT);
+            $result= $stmt->execute();
+            $arrayResult = [];
+
+            while($row=$result->fetchArray(SQLITE3_NUM)){ // how to read the result from the query
+              $arrayResult = $row;                              
+            }
+
+            else{
+              echo"invalid login";
+            }
+
+
+          }
+      }
+      ?>
+
+</body>
+
+<a href="SupplierLogin.php">
+   <input style="position:  absolute; top: 95%;" type="button" value="SupplierLogin" />
+</a>
+
+</html>
