16054e702255e27db50d64c0d9fbcd3331f3c2c5
Guy-Nicklin <100151534+Guy-Nicklin@users.noreply.github.com>
2022-03-16 16:50:02 +0000

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -41,56 +41,6 @@ function invalidLN($lName)
     }
     return $result;
 }
-//Function to check for invalid email addresses.
-function invalidEmail($email)
-{
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $result = true;
-    } else {
-        $result = false;
-    }
-    return $result;
-}
-//Function to check if the inputted email is already in use.
-function emailMatch($email)
-{
-    //this variable is used to indicate the creation is successfull or not.
-    $sqliteDebug = true;
-    try {
-        // attempt connection.
-        $db = new SQLite3('/Applications/XAMPP/data/publicnewsapplication.db');
-    } catch (Exception $exception) {
-        // sqlite3 throws an exception when it is unable to connect.
-        echo '<p>There was an error connecting to the database!</p>';
-        if ($sqliteDebug) {
-            echo $exception->getMessage();
-        }
-    }
-    //SQL select statement to identify customer details.
-    $sql = "SELECT * FROM Member";
-    $stmt = $db->prepare($sql);
-    $result = $stmt->execute();
-
-    //prepare an empty array 
-    $arrayResult = [];
-    //Itereate through results to find matching details.
-    while ($row = $result->fetchArray()) {
-        $arrayResult[] = $row;
-    }
-    for ($i = 0; $i < count($arrayResult); $i++) :
-        $emailExists = $arrayResult[$i]['Email'];
-
-
-    endfor;
-    //Compare inputed email with records taken from the database.
-    if ($email == $emailExists) {
-        $result = true;
-    } else {
-        $result = false;
-    }
-    return $result;
-}
 //Function to check that the password and re-entered password match.
 function passwordMismatch($pword, $rePassword)
 {
@@ -101,15 +51,16 @@ function passwordMismatch($pword, $rePassword)
     }
     return $result;
 }
+
 //Function to create a customer and assign to member table in the database.
-function createCustomer($fName, $lName, $email, $pword, $adminId)
+function createEngineer($fName, $lName, $pword, $groupID, $engineerRate)
 {
 
     //this variable is used to indicate the creation is successfull or not.
     $sqliteDebug = true;
     try {
         // attempt connection.
-        $db = new SQLite3('/Applications/XAMPP/data/publicnewsapplication.db');
+        $db = new SQLite3('C:\xampp\htdocs\myDB.db');
     }
     //catch exceptions.
     catch (Exception $exception) {
@@ -120,15 +71,15 @@ function createCustomer($fName, $lName, $email, $pword, $adminId)
         }
     }
     //SQL insert statement for insertion into the member table of the database.
-    $sql = "INSERT INTO Member(First_name,Last_name, Email, Member_password, Admin_id) VALUES (:fName, :lName, :email, :pword, :adminId)";
+    $sql = "INSERT INTO Engineer(F_name,L_name, Password, Group_ID, Engineer_rate) VALUES (:fName, :lName, :pword, groupId :engineerRate, )";
     //prepare the SQL statement.
     $stmt = $db->prepare($sql);
     //give the values for the parameters.
     $stmt->bindParam(':fName', $fName, SQLITE3_TEXT);
     $stmt->bindParam(':lName', $lName, SQLITE3_TEXT);
-    $stmt->bindParam(':email', $email, SQLITE3_TEXT);
     $stmt->bindParam(':pword', $pword, SQLITE3_TEXT);
-    $stmt->bindParam(':adminId', $adminId, SQLITE3_TEXT);
+    $stmt->bindParam(':groupId',$groupID , SQLITE3_TEXT);
+    $stmt->bindParam(':engineerRate', $adminId, SQLITE3_TEXT);
 
 
     //execute the sql statement.
@@ -145,97 +96,4 @@ function createCustomer($fName, $lName, $email, $pword, $adminId)
 
     return $created;
 }
-//The following functions apply to the customer login form.
-//Function to check if user inputs are left empty.
-function emptyCustomerLogin($userEmail, $userPassword)
-{
-    if (empty($userEmail)  || empty($userPassword)) {
-        $result = true;
-    } else {
-        $result = false;
-    }
-    return $result;
-}
-function loginCustomer($userEmail, $userPassword)
-{
-    //this variable is used to indicate the creation is successfull or not.
-    $sqliteDebug = true;
-    try {
-        // attempt connection.
-        $db = new SQLite3('/Applications/XAMPP/data/publicnewsapplication.db');
-    } catch (Exception $exception) {
-        // sqlite3 throws an exception when it is unable to connect.
-        echo '<p>There was an error connecting to the database!</p>';
-        if ($sqliteDebug) {
-            echo $exception->getMessage();
-        }
-    }
-    //SQL select statement to identify customer details.
-    $sql = "SELECT * FROM Member";
-    $stmt = $db->prepare($sql);
-    $result = $stmt->execute();
 
-    //prepare an empty array 
-    $arrayResult = [];
-    //Itereate through results to find matching details.
-    while ($row = $result->fetchArray()) {
-        $arrayResult[] = $row;
-    }
-    for ($i = 0; $i < count($arrayResult); $i++) :
-        $emailExists = $arrayResult[$i]['Email'];
-        $pwdExists = $arrayResult[$i]['Member_password'];
-
-    endfor;
-    if (($userEmail !== $emailExists) && ($userPassword !== $pwdExists)) {
-        $result = true;
-    } else {
-        $result = false;
-    }
-    return $result;
-}
-//The following functions apply to the Admin login form.
-//Function to check if user inputs are left empty.
-function emptyAdminLogin($adminUsername, $adminPassword)
-{
-    if (empty($adminUsername)  || empty($adminPassword)) {
-        $result = true;
-    } else {
-        $result = false;
-    }
-    return $result;
-}
-function loginUser($adminUsername, $adminPassword)
-{
-    //this variable is used to indicate the creation is successfull or not
-    $sqliteDebug = true;
-    try {
-        // attempt connection
-        $db = new SQLite3('/Applications/XAMPP/data/publicnewsapplication.db');
-    } catch (Exception $exception) {
-        // sqlite3 throws an exception when it is unable to connect
-        echo '<p>There was an error connecting to the database!</p>';
-        if ($sqliteDebug) {
-            echo $exception->getMessage();
-        }
-    }
-
-    $sql = "SELECT * FROM Admin";
-    $stmt = $db->prepare($sql);
-    $result = $stmt->execute();
-
-    $arrayResult = []; //prepare an empty array first
-    while ($row = $result->fetchArray()) { // use fetchArray(SQLITE3_NUM) - another approach
-        $arrayResult[] = $row; //adding a record until end of records
-    }
-    for ($i = 0; $i < count($arrayResult); $i++) :
-        $nameExists = $arrayResult[$i]['Username'];
-        $pwdExists = $arrayResult[$i]['Admin_password'];
-
-    endfor;
-    if (($adminUsername !== $nameExists) && ($adminPassword !== $pwdExists)) {
-        $result = true;
-    } else {
-        $result = false;
-    }
-    return $result;
-}
