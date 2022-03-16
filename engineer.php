57222a247a1398a9d47959b7a62e2760d6581296
Guy-Nicklin <100151534+Guy-Nicklin@users.noreply.github.com>
2022-03-16 16:55:43 +0000

    Update engineer.php

--- engineer.php
+++ engineer.php
@@ -1,19 +1,7 @@
 <?php
 require("require.php");
 
-$db = new SQLITE3('mydb.db');
-$sql = "SELECT * 
-        FROM Project 
-        WHERE Project_id = 1";
 
-
-$stmt = $db->prepare($sql);
-$result = $stmt->execute();
-
-$arrayResult = []; //prepare an empty array first
-while ($row = $result->fetchArray()) { // use fetchArray(SQLITE3_NUM) - another approach
-    $arrayResult[] = $row; //adding a record until end of records
-}
 
 ?>
 
