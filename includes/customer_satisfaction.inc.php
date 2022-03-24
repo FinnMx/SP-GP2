a8df432efcb0deddec52216f8c4b8975060a289b
Guy-Nicklin <100151534+Guy-Nicklin@users.noreply.github.com>
2022-03-24 10:20:26 +0000

    Update status

--- includes/customer_satisfaction.inc.php
+++ includes/customer_satisfaction.inc.php
@@ -1,33 +1,33 @@
 <?php
- if(isset($_POST['submitCS'])){
- 	SetCustomerSatisfaction($_POST['project_id'],$_POST['customer_satisfaction']);
- }
+if (isset($_POST['submitCS'])) {
+   SetCustomerSatisfaction($_POST['project_id'], $_POST['customer_satisfaction']);
+}
 
 
- function SetCustomerSatisfaction($PID,$CS){
+function SetCustomerSatisfaction($PID, $CS)
+{
    $user_agent = getenv("HTTP_USER_AGENT");
 
-   if(strpos($user_agent, "Win") !== FALSE)
-   $os = "Windows";
-   elseif(strpos($user_agent, "Mac") !== FALSE)
-   $os = "Mac";
+   if (strpos($user_agent, "Win") !== FALSE)
+      $os = "Windows";
+   elseif (strpos($user_agent, "Mac") !== FALSE)
+      $os = "Mac";
 
-   if($os === "Windows")
-   {
+   if ($os === "Windows") {
       $db = new SQLite3('C:\xampp\htdocs\myDB.db');
-   }
-   elseif($os === "Mac")
-   {
+   } elseif ($os === "Mac") {
       $db = new SQLite3('/Applications/XAMPP/data/myDB.db');
-   } 
-   ;
+   };
 
- 	$sql = "UPDATE Project SET Customer_satisfaction =:cs WHERE Project_ID =:pid";
+   $status = "complete";
+
+   $sql = "UPDATE Project SET Customer_satisfaction =:cs AND Status =:status WHERE Project_ID =:pid";
    $stmt = $db->prepare($sql);
    $stmt->bindParam(':cs', $CS, SQLITE3_TEXT);
+   $stmt->bindParam(':status', $status, SQLITE3_TEXT);
    $stmt->bindParam(':pid', $PID, SQLITE3_TEXT);
    $result = $stmt->execute();
 
 
-    header("Location: ..\Manager.php");
- }
+   header("Location: ../manager.php");
+}
