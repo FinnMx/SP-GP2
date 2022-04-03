d801b8a301db0984b86b7fc57fa0d2dd7f1e6bb5
Guy-Nicklin <100151534+Guy-Nicklin@users.noreply.github.com>
2022-04-03 13:16:52 +0100

    should work now :/

--- includes/customer_satisfaction.inc.php
+++ includes/customer_satisfaction.inc.php
@@ -1,22 +1,16 @@
 <?php
 //needs cs comments to do something
 if (isset($_POST['submitCS'])) {
-   if (isset($_POST['submitCS']))
-   {
+   if (isset($_POST['submitCS'])) {
 
       $PID = $_POST['project_id'];
       $CS = $_POST['customer_satisfaction'];
       if ($PID == '' || $CS == '') {
          header("Location: ../manager.php?errorcs=emptyinput");
          exit();
-      }
-      else
-      {
+      } else {
+         require_once("functions.inc.php");
          SetCustomerSatisfaction($PID, $CS);
       }
-
    }
-   
-   
 }
-
